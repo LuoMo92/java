53af3241e020e424548ff565f6c259b93b807f4a
LuoMo92 <810754420@qq.com>
2018-03-02 16:58:27 +0800

    Initial commit

---
+++ src/main/java/com/luomo/java/base/NoVisibility.java
@@ -0,0 +1,28 @@
+package com.luomo.java.base;
+
+/**
+ * @author LiuMei
+ * @date 2018-03-02.
+ */
+public class NoVisibility {
+
+    private static boolean ready;
+    private static int number;
+
+    private static class ReaderThread extends Thread {
+        @Override
+        public void run() {
+            while (!ready) {
+                Thread.yield();
+            }
+            System.out.println(number);
+        }
+    }
+
+    public static void main(String[] args) {
+        new ReaderThread().start();
+        number = 42;
+        ready = true;
+    }
+
+}
