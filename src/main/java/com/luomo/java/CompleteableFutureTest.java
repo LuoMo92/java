cc9bb8c64146307a7c20d682569203abf31dcc09
luomo92 <810754420@qq.com>
2019-04-28 16:59:11 +0800

    异步编程测试类

---
+++ src/main/java/com/luomo/java/CompleteableFutureTest.java
@@ -0,0 +1,51 @@
+package com.luomo.java;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author LiuMei
+ * @date 2019/4/28 0028
+ */
+public class CompleteableFutureTest {
+
+    void sleep(int t, TimeUnit u) {
+        try {
+            u.sleep(t);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        CompleteableFutureTest test = new CompleteableFutureTest();
+        /**
+         * 任务1：洗水壶-》烧开水
+         */
+        CompletableFuture<Void> f1 = CompletableFuture.runAsync(() -> {
+            System.out.println("T1:洗水壶。。。");
+            test.sleep(1,TimeUnit.SECONDS);
+            System.out.println("T1:烧开水。。。");
+            test.sleep(15, TimeUnit.SECONDS);
+        });
+
+        CompletableFuture<String> f2 = CompletableFuture.supplyAsync(() -> {
+            System.out.println("T2:洗茶壶。。。");
+            test.sleep(1, TimeUnit.SECONDS);
+            System.out.println("T2:洗茶杯。。。");
+            test.sleep(2, TimeUnit.SECONDS);
+            System.out.println("T2:拿茶叶。。。");
+            test.sleep(1, TimeUnit.SECONDS);
+            return "龙井";
+        });
+
+        CompletableFuture<String> f3 = f1.thenCombine(f2, (__, tf) -> {
+            System.out.println("T3:拿到茶叶:" + tf);
+            System.out.println("T3:泡茶。。。");
+            return "上茶：" + tf;
+        });
+        System.out.println(f3.join());
+    }
+
+
+}
---
+++ src/main/java/com/luomo/java/CompletionServiceTest.java
@@ -0,0 +1,36 @@
+package com.luomo.java;
+
+import java.util.concurrent.*;
+
+/**
+ * @author LiuMei
+ * @date 2019/4/28 0028
+ */
+public class CompletionServiceTest {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        CompletionService<Integer> cs = new ExecutorCompletionService<>(executor);
+        cs.submit(() -> {
+                    System.out.println("S1询价");
+                    return 1;
+                }
+        );
+        cs.submit(() -> {
+                    System.out.println("S2询价");
+                    return 2;
+                }
+        );
+        cs.submit(() -> {
+                    System.out.println("S3询价");
+                    return 3;
+                }
+        );
+        for(int i=0;i<3;i++){
+            Integer price = cs.take().get();
+            executor.execute(()->{
+                System.out.println("保存"+price);
+            });
+        }
+    }
+}
