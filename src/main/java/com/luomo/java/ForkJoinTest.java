f5397f07adab52fbdeab3181423c380a847e3781
luomo92 <810754420@qq.com>
2019-04-29 15:03:53 +0800

    Fork/Join测试类

---
+++ src/main/java/com/luomo/java/ForkJoinTest.java
@@ -0,0 +1,40 @@
+package com.luomo.java;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+
+/**
+ * @author LiuMei
+ * @date 2019/4/29 0029
+ */
+public class ForkJoinTest {
+
+    public static void main(String[] args) {
+        ForkJoinPool fjp = new ForkJoinPool(4);
+        Fibonacci fib = new Fibonacci(30);
+        Integer result = fjp.invoke(fib);
+        System.out.println(result);
+    }
+
+    static class Fibonacci extends RecursiveTask<Integer> {
+        final int n;
+
+        Fibonacci(int n) {
+            this.n = n;
+        }
+
+        @Override
+        protected Integer compute() {
+            if (n < 1) {
+                return n;
+            }
+            Fibonacci f1 = new Fibonacci(n - 1);
+            f1.fork();
+            Fibonacci f2 = new Fibonacci(n - 2);
+            return f2.compute() + f1.join();
+        }
+    }
+
+}
+
+
